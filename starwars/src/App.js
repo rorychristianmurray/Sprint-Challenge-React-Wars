8f3446a5540d89f95a1f07442d5931c7584a8518
Rory Murray <rory.christian.murray@gmail.com>
2019-07-12 11:49:00 -0400

    Finish App.js, wireframe component structure

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import CharacterList from "./CharacterList";
 import "./App.css";
 
 const App = () => {
@@ -23,9 +24,14 @@ const App = () => {
       });
   };
 
+  useEffect(() => {
+    fetchChars();
+  }, []);
+
   return (
     <div className="App">
       <h1 className="Header">React Wars</h1>
+      <CharacterList chars={chars} />
     </div>
   );
 };
